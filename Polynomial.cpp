a4e1de5b55708508b8c304779f3e6e555caad52a
tfpf <vpaijc@gmail.com>
2020-11-28 01:06:32 +0530

    i keep making typos for some reason

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -507,7 +507,7 @@ std::string rationalise(double real, ilong max_denominator)
 
     // obtain a rational approximation with a large denominator
     // if, after removing the GCD, the denominator is small, that's the answer
-    // if plain `int' is used, `real * d' may result in integer overflow)
+    // if plain `int' is used, `real * d' may result in integer overflow
     // hence, use the `ilong' typedef
     ilong d = 1e12;
     ilong n = real * d;
